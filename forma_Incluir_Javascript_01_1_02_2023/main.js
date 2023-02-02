b50fe56795d2b285bc04ecb52a3ea1ad4363dd26
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-01 21:11:48 -0500

    Solucion Ejercicio 1

---
+++ forma_Incluir_Javascript_01_1_02_2023/main.js
@@ -0,0 +1,6 @@
+//Haz un programa de nombre main.js y otro llamado index.html 
+//incluir el archivo JavaScript en el <head></ head>, que has de grabar en una carpeta 
+//llamada forma_Incluir_Javascript_01_FechaActual, mostrar un mensaje por medio de la consola o 
+//de la ventana emergente un mensaje “Conexión con el archivo JavaScript Exitoso” 
+
+console.log("%cConexión con el archivo JavaScrip Exitoso","font-family:cursive;");
