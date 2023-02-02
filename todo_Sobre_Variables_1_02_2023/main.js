aa853a232edfe065fe7d24d6ee39acc037a5b67c
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-01 21:18:12 -0500

    Solucion Ejercicio 4

---
+++ todo_Sobre_Variables_1_02_2023/main.js
@@ -0,0 +1,8 @@
+let Mensaje = "Esta es la clase de JavaScript ";
+let Numero = 6895.8;
+let Entendido = true;
+
+console.log("El contenido de la variable string es: ", Mensaje);
+console.log("El contenido de la variable numérica es: " ,Numero);
+console.log("El contenido de la variable Booleana es: ", Entendido);
+
